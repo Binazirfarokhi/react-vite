0709ab343c06e2f6c8c69f1d5dd643771a8414e2
Binazir <binazir.farokhi@gmail.com>
2025-02-22 11:12:12 -0800

    set up

---
+++ index.jsx
@@ -0,0 +1,8 @@
+import { createElement } from "react"
+
+import {createRoot} from "react-dom/client"
+
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(createElement("h1", null, "hello from createlement"))
