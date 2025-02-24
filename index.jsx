3a138eebaec74e2775a4a5d1d27a5bb361e7e682
Binazir <binazir.farokhi@gmail.com>
2025-02-23 22:06:04 -0800

    last

--- index.jsx
+++ index.jsx
@@ -2,20 +2,18 @@ import{createElement} from "react"
 import {createRoot} from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
 root.render(
-    <div>
-        <h1>React App</h1>
-        <p>React App</p>
-        <ul>
-            <li>Home </li>
-            <li>About us </li>
-            <li>Contact </li>
-        </ul>
-    </div>
+    <TemporaryName/>
   
 )
 
-function temporaryName(){ 
-    
+function TemporaryName(){ 
+    return(
+        <main>
+            <img src="" alt="" />
+            <h1>Fun Facts about React </h1>
+        </main>
+    )
+
 }
 
 
